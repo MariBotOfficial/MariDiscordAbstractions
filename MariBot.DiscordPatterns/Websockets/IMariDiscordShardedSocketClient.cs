a4a220f7b782422c9ca33d252989c842d1b84c35
renanrcp <renancamerapinheiro@gmail.com>
2020-05-07 23:08:51 -0300

    base client interfaces

---
+++ MariBot.DiscordPatterns/Websockets/IMariDiscordShardedSocketClient.cs
@@ -0,0 +1,10 @@
+namespace MariBot.DiscordPatterns.Websockets
+{
+    /// <summary>
+    /// Base sharded socket client interface.
+    /// </summary>
+    public interface IMariDiscordShardedSocketClient : IMariDiscordSocketClient
+    {
+         
+    }
+}
---
+++ MariBot.DiscordPatterns/Websockets/IMariDiscordSocketClient.cs
@@ -0,0 +1,10 @@
+namespace MariBot.DiscordPatterns.Websockets
+{
+    /// <summary>
+    /// Base socket client interface.
+    /// </summary>
+    public interface IMariDiscordSocketClient
+    {
+        
+    }
+}
