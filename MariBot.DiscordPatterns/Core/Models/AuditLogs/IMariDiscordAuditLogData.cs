f3f81b6d574fd553145b8ef13e1c9e034aa21fc6
renanrcp <renancamerapinheiro@gmail.com>
2020-05-30 15:25:27 -0300

    IMariDiscordAuditLogEntry

---
+++ MariBot.DiscordPatterns/Core/Models/AuditLogs/IMariDiscordAuditLogData.cs
@@ -0,0 +1,9 @@
+namespace MariBot.DiscordPatterns.Core.Models.AuditLogs
+{
+    /// <summary>
+    /// Represents data applied to an <see cref="IMariDiscordAuditLogEntry"/>.
+    /// </summary>
+    public interface IMariDiscordAuditLogData
+    {
+    }
+}
---
+++ MariBot.DiscordPatterns/Core/Models/AuditLogs/IMariDiscordAuditLogEntry.cs
@@ -0,0 +1,30 @@
+using MariBot.DiscordPatterns.Core.Models.Users;
+
+namespace MariBot.DiscordPatterns.Core.Models.AuditLogs
+{
+    /// <summary>
+    /// Represents a generic audit log entry.
+    /// </summary>
+    public interface IMariDiscordAuditLogEntry : IMariDiscordSnowFlakeEntity
+    {
+        /// <summary>
+        /// The action which occurred to create this entry.
+        /// </summary>
+        MariDiscordActionType Action { get; }
+
+        /// <summary>
+        /// The data for this entry.
+        /// </summary>
+        IMariDiscordAuditLogData Data { get; }
+
+        /// <summary>
+        /// The user responsible for causing the changes.
+        /// </summary>
+        IMariDiscordUser User { get; }
+
+        /// <summary>
+        /// The reason behind the change.
+        /// </summary>
+        string Reason { get; }
+    }
+}
