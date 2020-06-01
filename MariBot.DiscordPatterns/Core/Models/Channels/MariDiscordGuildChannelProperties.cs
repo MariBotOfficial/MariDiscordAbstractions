1f5283bab44132248ccb5cb880ce8c668f2d7d2e
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:07:41 -0300

    optional in MariDiscordGuildChannelProperties

--- MariBot.DiscordPatterns/Core/Models/Channels/MariDiscordGuildChannelProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Channels/MariDiscordGuildChannelProperties.cs
@@ -8,16 +8,16 @@ namespace MariBot.DiscordPatterns.Core.Models.Channels
         /// <summary>
         /// Gets or sets the channel to this name.
         /// </summary>
-        public string Name { get; set; }
+        public MariDiscordOptional<string> Name { get; set; }
 
         /// <summary>
         /// Moves the channel to the following position. This property is zero-based.
         /// </summary>
-        public int? Position { get; set; }
+        public MariDiscordOptional<int> Position { get; set; }
 
         /// <summary>
         /// Gets or sets the category ID for this channel.
         /// </summary>
-        public ulong? CategoryId { get; set; }
+        public MariDiscordOptional<ulong?> CategoryId { get; set; }
     }
 }
