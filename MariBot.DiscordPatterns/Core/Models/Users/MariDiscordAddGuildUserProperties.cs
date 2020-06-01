add5f57804290b298e178f687c5c968199ce7c7e
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:55:34 -0300

    MariDiscordOptional for MariDiscordAddGuildUserProperties

--- MariBot.DiscordPatterns/Core/Models/Users/MariDiscordAddGuildUserProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Users/MariDiscordAddGuildUserProperties.cs
@@ -11,26 +11,26 @@ namespace MariBot.DiscordPatterns.Core.Models.Users
         /// <summary>
         /// Gets or sets the user's nickname.
         /// </summary>
-        public string Nickname { get; set; }
+        public MariDiscordOptional<string> Nickname { get; set; }
 
         /// <summary>
         /// Gets or sets whether the user should be muted in a voice channel.
         /// </summary>
-        public bool? Mute { get; set; }
+        public MariDiscordOptional<bool> Mute { get; set; }
 
         /// <summary>
         /// Gets or sets whether the user should be deafened in a voice channel.
         /// </summary>
-        public bool? Deaf { get; set; }
+        public MariDiscordOptional<bool> Deaf { get; set; }
 
         /// <summary>
         /// Gets or sets the roles the user should have.
         /// </summary>
-        public IEnumerable<IMariDiscordRole> Roles { get; set; }
+        public MariDiscordOptional<IEnumerable<IMariDiscordRole>> Roles { get; set; }
 
         /// <summary>
         /// Gets or sets the roles the user should have.
         /// </summary>
-        public IEnumerable<ulong> RoleIds { get; set; }
+        public MariDiscordOptional<IEnumerable<ulong>> RoleIds { get; set; }
     }
 }
