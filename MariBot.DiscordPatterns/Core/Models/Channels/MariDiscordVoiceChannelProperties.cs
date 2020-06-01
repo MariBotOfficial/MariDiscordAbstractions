79cf8aa989ea526fbd0b3186a3b3cd2e39875b07
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:10:14 -0300

    MariDiscordOptional in MariDiscordVoiceChannelProperties

--- MariBot.DiscordPatterns/Core/Models/Channels/MariDiscordVoiceChannelProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Channels/MariDiscordVoiceChannelProperties.cs
@@ -8,11 +8,11 @@ namespace MariBot.DiscordPatterns.Core.Models.Channels
         /// <summary>
         /// Gets or sets the bitrate of the voice connections in this channel. Must be greater than 8000.
         /// </summary>
-        public int? BitRate { get; set; }
+        public MariDiscordOptional<int> BitRate { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum number of users that can be present in a channel, or <c>null</c> if none.
         /// </summary>
-        public int? UserLimit { get; set; }
+        public MariDiscordOptional<int?> UserLimit { get; set; }
     }
 }
