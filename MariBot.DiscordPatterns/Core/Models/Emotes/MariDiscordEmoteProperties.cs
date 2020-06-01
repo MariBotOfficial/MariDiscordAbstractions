9dc6970bcb1f0c5659c5b24631e2972afa69ed8f
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:17:52 -0300

    MariDiscordOptional for MariDiscordOptional.

--- MariBot.DiscordPatterns/Core/Models/Emotes/MariDiscordEmoteProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Emotes/MariDiscordEmoteProperties.cs
@@ -6,16 +6,16 @@ namespace MariBot.DiscordPatterns.Core.Models.Emotes
     /// <summary>
     /// Provides properties that are used to modify an <see cref="IMariDiscordEmote" /> with the specified changes.
     /// </summary>
-    public class MariDiscordEmoteProperties
+    public class MariDiscordOptional
     {
         /// <summary>
         /// Gets or sets the name of the <see cref="IMariDiscordEmote"/>.
         /// </summary>
-        public string Name { get; set; }
+        public MariDiscordOptional<string> Name { get; set; }
 
         /// <summary>
         /// Gets or sets the roles that can access this <see cref="IMariDiscordEmote"/>.
         /// </summary>
-        public IEnumerable<IMariDiscordRole> Roles { get; set; }
+        public MariDiscordOptional<IEnumerable<IMariDiscordRole>> Roles { get; set; }
     }
 }
