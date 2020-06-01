87bc34402941863c02b0ca1280a77b58fc6128c2
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:22:44 -0300

    MariDiscordOptional for MariDiscordGuildEmbedProperties.

--- MariBot.DiscordPatterns/Core/Models/Guilds/MariDiscordGuildEmbedProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Guilds/MariDiscordGuildEmbedProperties.cs
@@ -10,16 +10,16 @@ namespace MariBot.DiscordPatterns.Core.Models.Guilds
         /// <summary>
         /// Gets or sets whether the widget should be enabled.
         /// </summary>
-        public bool? Enabled { get; set; }
+        public MariDiscordOptional<bool> Enabled { get; set; }
 
         /// <summary>
         /// Gets or sets the channel that the invite should place its users in, if not <c>null</c>.
         /// </summary>
-        public IMariDiscordChannel Channel { get; set; }
+        public MariDiscordOptional<IMariDiscordChannel> Channel { get; set; }
 
         /// <summary>
         /// Gets or sets the channel the invite should place its users in, if not <c>null</c>.
         /// </summary>
-        public ulong? ChannelId { get; set; }
+        public MariDiscordOptional<ulong?> ChannelId { get; set; }
     }
 }
