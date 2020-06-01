b5a841fddcfb347053339efc3136951a6dd02255
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 23:57:06 -0300

    MariDiscordOptional for MariDiscordGuildUserProperties

--- MariBot.DiscordPatterns/Core/Models/Users/MariDiscordGuildUserProperties.cs
+++ MariBot.DiscordPatterns/Core/Models/Users/MariDiscordGuildUserProperties.cs
@@ -12,39 +12,36 @@ namespace MariBot.DiscordPatterns.Core.Models.Users
         /// <summary>
         /// Gets or sets whether the user should be muted in a voice channel.
         /// </summary>
-        public bool? Mute { get; set; }
+        public MariDiscordOptional<bool> Mute { get; set; }
 
         /// <summary>
         /// Gets or sets whether the user should be deafened in a voice channel.
         /// </summary>
-        public bool? Deaf { get; set; }
+        public MariDiscordOptional<bool> Deaf { get; set; }
 
         /// <summary>
         /// Gets or sets the user's nickname.
         /// </summary>
-        public string Nickname { get; set; }
+        public MariDiscordOptional<string> Nickname { get; set; }
 
         /// <summary>
         /// Gets or sets the roles the user should have.
         /// </summary>
-        public IEnumerable<IMariDiscordRole> Roles { get; set; }
+        public MariDiscordOptional<IEnumerable<IMariDiscordRole>> Roles { get; set; }
 
         /// <summary>
         /// Gets or sets the roles the user should have.
         /// </summary>
-        public IEnumerable<ulong> RoleIds { get; set; }
+        public MariDiscordOptional<IEnumerable<ulong>> RoleIds { get; set; }
 
         /// <summary>
         /// Moves a user to a voice channel. If <c>null</c>, this user will be disconnected from their current voice channel.
         /// </summary>
-        public IMariDiscordVoiceChannel Channel { get; set; }
+        public MariDiscordOptional<IMariDiscordVoiceChannel> Channel { get; set; }
 
         /// <summary>
         /// Moves a user to a voice channel. Set <see cref="Channel"/> to <c>null</c> to disconnect this user from their current voice channel.
         /// </summary>
-        /// <note>
-        /// If you don't set the ChannelId the user will be kicked from the voice channel.
-        /// </note>
-        public ulong? ChannelId { get; set; }
+        public MariDiscordOptional<ulong?> ChannelId { get; set; }
     }
 }
